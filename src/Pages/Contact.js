76906abf436cad88dee08cb6f86d4e796755d3b7
Izzy V <israelreyvarela@gmail.com>
2020-12-03 15:54:00 -0600

    added contact cards to contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,38 +1,43 @@
-import React from 'react';
-import Color from 'color';
-import GoogleFont from 'react-google-font-loader';
-import { makeStyles } from '@material-ui/core/styles';
-import NoSsr from '@material-ui/core/NoSsr';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import { useFourThreeCardMediaStyles } from '@mui-treasury/styles/cardMedia/fourThree';
+import React from "react";
+import Color from "color";
+import GoogleFont from "react-google-font-loader";
+import { makeStyles } from "@material-ui/core/styles";
+import NoSsr from "@material-ui/core/NoSsr";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import { useFourThreeCardMediaStyles } from "@mui-treasury/styles/cardMedia/fourThree";
+import { AutoComplete } from "material-ui";
 
 const useGridStyles = makeStyles(({ breakpoints }) => ({
   root: {
-    [breakpoints.up('md')]: {
-      justifyContent: 'center',
+    [breakpoints.up("md")]: {
+      justifyContent: "center",
     },
   },
 }));
 
 const useStyles = makeStyles(() => ({
   actionArea: {
+      paddingTop: 60,
     borderRadius: 16,
-    transition: '0.2s',
-    '&:hover': {
-      transform: 'scale(1.1)',
+    transition: "0.2s",
+    "&:hover": {
+      transform: "scale(1.1)",
     },
   },
   card: ({ color }) => ({
+    
     minWidth: 256,
+    maxWidth:256,
+    
     borderRadius: 16,
-    boxShadow: 'none',
-    '&:hover': {
-      boxShadow: `0 6px 12px 0 ${Color(color)
+    boxShadow: "none",
+    "&:hover": {
+      boxShadow: `0 3px 6px 0 ${Color(color)
         .rotate(-12)
         .darken(0.2)
         .fade(0.5)}`,
@@ -41,20 +46,19 @@ const useStyles = makeStyles(() => ({
   content: ({ color }) => {
     return {
       backgroundColor: color,
-      padding: '1rem 1.5rem 1.5rem',
+      padding: "1rem 1.5rem 1.5rem",
     };
   },
   title: {
-    fontFamily: 'Keania One',
-    fontSize: '2rem',
-    color: '#fff',
-    textTransform: 'uppercase',
+    fontFamily: "Keania One",
+    fontSize: "2rem",
+    color: "#fff",
   },
   subtitle: {
-    fontFamily: 'Montserrat',
-    color: '#fff',
+    fontFamily: "Montserrat",
+    color: "#fff",
     opacity: 0.87,
-    marginTop: '2rem',
+    marginTop: "2rem",
     fontWeight: 500,
     fontSize: 14,
   },
@@ -67,7 +71,7 @@ const CustomCard = ({ classes, image, title, subtitle }) => {
       <Card className={classes.card}>
         <CardMedia classes={mediaStyles} image={image} />
         <CardContent className={classes.content}>
-          <Typography className={classes.title} variant={'h2'}>
+          <Typography className={classes.title} variant={"h2"}>
             {title}
           </Typography>
           <Typography className={classes.subtitle}>{subtitle}</Typography>
@@ -79,48 +83,44 @@ const CustomCard = ({ classes, image, title, subtitle }) => {
 
 export const ContactMe = React.memo(function ContactMe() {
   const gridStyles = useGridStyles();
-  const styles = useStyles({ color: '#203f52' });
-  const styles2 = useStyles({ color: '#4d137f' });
-  const styles3 = useStyles({ color: '#ff9900' });
-  const styles4 = useStyles({ color: '#34241e' });
+  const styles = useStyles({ color: "#00000" });
+//   const styles2 = useStyles({ color: "#4d137f" });
+//   const styles3 = useStyles({ color: "#ff9900" });
+//   const styles4 = useStyles({ color: "#34241e" });
   return (
     <>
-      <Grid classes={gridStyles} container spacing={4} wrap={'nowrap'}>
+      <Grid classes={gridStyles} container spacing={10} wrap={"nowrap"}>
         <Grid item>
           <CustomCard
             classes={styles}
-            title={'Dota 2'}
-            subtitle={'Be a Legend!'}
+            title={"Connect on GitHub"}
+            subtitle={
+              "Visit me on GitHub page to see my code work, and connect with me."
+            }
             image={
-              'https://steamcdn-a.akamaihd.net/apps/dota2/images/blog/play/dota_heroes.png'
+              "./israelVarelaCommercialHS.jpg"
             }
           />
         </Grid>
         <Grid item>
           <CustomCard
-            classes={styles2}
-            title={'Fortnite'}
-            subtitle={'Time to choose side!'}
+            classes={styles}
+            title={"LinkedIn"}
+            subtitle={
+              "Visit me on LinkedIn to connect and see my work history."
+            }
             image={
-              'https://progameguides.com/wp-content/uploads/2019/10/fortnite-outfit-scratch.jpg'
+              "/Users/israelvarela/Desktop/Code/COPY - smu-dal-fsf-pt-06-2020-u-c /20-State/02-Homework/react-portfolio/src/images2/israelVarelaCommercialHS.jpg"
             }
           />
         </Grid>
         <Grid item>
           <CustomCard
-            classes={styles3}
-            title={'Overwatch'}
-            subtitle={'What are you waiting?'}
-            image={'https://images5.alphacoders.com/690/thumb-1920-690653.png'}
-          />
-        </Grid>
-        <Grid item>
-          <CustomCard
-            classes={styles4}
-            title={'PUBG'}
-            subtitle={'Are you ready?'}
+            classes={styles}
+            title={"Resume"}
+            subtitle={"View and download resume."}
             image={
-              'https://www.itp.net/public/styles/full_img_sml/public/images/2019/05/27/44485-pubg_base1.jpg?itok=EF911Xan'
+              "/Users/israelvarela/Desktop/Code/COPY - smu-dal-fsf-pt-06-2020-u-c /20-State/02-Homework/react-portfolio/src/images2/israelVarelaCommercialHS.jpg"
             }
           />
         </Grid>
@@ -128,4 +128,4 @@ export const ContactMe = React.memo(function ContactMe() {
     </>
   );
 });
-export default ContactMe
+export default ContactMe;
