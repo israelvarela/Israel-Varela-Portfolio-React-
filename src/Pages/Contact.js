29e5524cf52395493378301ff8b21b162551b2ee
Izzy V <israelreyvarela@gmail.com>
2020-12-03 23:16:26 -0600

    updating contact page code

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -11,10 +11,11 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import { useFourThreeCardMediaStyles } from "@mui-treasury/styles/cardMedia/fourThree";
 import { AutoComplete } from "material-ui";
-import Link from '@material-ui/core/Link';
-import image from '../images2/israelVarelaCommercialHS.jpg'
-import image2 from '../images2/israelVarelaHS2.jpg'
-import image3 from '../images2/israelVarelaHS3.jpg'
+import Link from "@material-ui/core/Link";
+import image from "../images2/israelVarelaCommercialHS.jpg";
+import image2 from "../images2/israelVarelaHS2.jpg";
+import image3 from "../images2/israelVarelaHS3.jpg";
+import image4 from "../images2/israelVarelaHS4.jpg";
 import "../components/Header/style.css";
 import "./style.css";
 
@@ -23,12 +24,13 @@ const useGridStyles = makeStyles(({ breakpoints }) => ({
     [breakpoints.up("md")]: {
       justifyContent: "center",
     },
+    flexGrow: 1,
   },
 }));
 
 const useStyles = makeStyles(() => ({
   actionArea: {
-      paddingTop: 110,
+    paddingTop: 110,
     borderRadius: 16,
     transition: "0.2s",
     "&:hover": {
@@ -36,9 +38,8 @@ const useStyles = makeStyles(() => ({
     },
   },
   card: ({ color }) => ({
-    
     minWidth: 256,
-    maxWidth:256,
+    maxWidth: 256,
     padding: 50,
     borderRadius: 16,
     boxShadow: "none",
@@ -76,8 +77,6 @@ const useStyles = makeStyles(() => ({
     fontWeight: 500,
     fontSize: 14,
   },
-
-  
 }));
 
 const CustomCard = ({ classes, image, title, subtitle, subtitle2 }) => {
@@ -85,7 +84,7 @@ const CustomCard = ({ classes, image, title, subtitle, subtitle2 }) => {
   return (
     <CardActionArea className={classes.actionArea}>
       <Card className={classes.card}>
-        <CardMedia classes={mediaStyles} image={image}  />
+        <CardMedia classes={mediaStyles} image={image} />
         <CardContent className={classes.content}>
           <Typography className={classes.title} variant={"h2"}>
             {title}
@@ -101,66 +100,67 @@ const CustomCard = ({ classes, image, title, subtitle, subtitle2 }) => {
 export const ContactMe = React.memo(function ContactMe() {
   const gridStyles = useGridStyles();
   const styles = useStyles({ color: "#000000" });
-//   const styles2 = useStyles({ color: "#4d137f" });
-//   const styles3 = useStyles({ color: "#ff9900" });
-//   const styles4 = useStyles({ color: "#34241e" });
+  //   const styles2 = useStyles({ color: "#4d137f" });
+  //   const styles3 = useStyles({ color: "#ff9900" });
+  //   const styles4 = useStyles({ color: "#34241e" });
   return (
-    <>
-      <Grid classes={gridStyles} container spacing={10} wrap={"nowrap"}>
-
-      <Link target="_blank" href="https://github.com/israelvarela" >
-        <Grid item xs={12} sm={6} md={3}>
-          <CustomCard
-            classes={styles}
-            title={"Connect on GitHub"}
-  
-            subtitle={
-              "Visit me on GitHub page to see my code work, and connect with me."
-            }
-            image={image}
-          />
-        </Grid>
-        </Link>
-        
-        <Link target="_blank" href="https://www.linkedin.com/in/israelrvarela/" >
-        <Grid item xs={12} sm={6} md={3}>
-          <CustomCard 
-            classes={styles}
-            title={"Connect on LinkedIn"}
-            subtitle={
-              "Visit me on LinkedIn to connect and see my work history."
-            }
-          
-            image={ image2 }
-          />
-        </Grid>
+    <div>
+      <Grid classes={gridStyles} container spacing={7} wrap={"nowrap"}>
+        <Link target="_blank" href="https://github.com/israelvarela">
+          <Grid item xs={6} sm={3}>
+            <CustomCard
+              classes={styles}
+              title={"Connect on GitHub"}
+              subtitle={
+                "Visit me on GitHub page to see my code work, and connect with me."
+              }
+              image={image}
+            />
+          </Grid>
         </Link>
 
-        <Link target="_blank" href="https://israelvarela.github.io/portfolio/assets/images/Israel_Varela_Resume_2020C.pdf" >
-        <Grid item xs={12} sm={6} md={3}>
-          <CustomCard
-            classes={styles}
-            title={"View Resume"}
-            subtitle={"View and download my resume to see my professional background and skill sets I've acquired throughout the course of my career."}
-            image={image3}
-          />
-        </Grid>
+        <Link target="_blank" href="https://www.linkedin.com/in/israelrvarela/">
+          <Grid item xs={6} sm={3}>
+            <CustomCard
+              classes={styles}
+              title={"Connect on LinkedIn"}
+              subtitle={
+                "Visit me on LinkedIn to connect and see my work history."
+              }
+              image={image2}
+            />
+          </Grid>
         </Link>
 
-        <Link target="_blank" href="mailto:israel.varela78@yahoo.com" >
-        <Grid item xs={12} sm={6} md={3}>
-          <CustomCard
-            classes={styles}
-            title={"Feel Free To Reach Out!"}
-            subtitle={"p: 214-677-7668"}
-            subtitle2={"e: israel.varela78@yahoo.com"}
-            image={image}
-          />
-        </Grid>
+        <Link
+          target="_blank"
+          href="https://israelvarela.github.io/portfolio/assets/images/Israel_Varela_Resume_2020C.pdf"
+        >
+          <Grid item xs={6} sm={3}>
+            <CustomCard
+              classes={styles}
+              title={"View Resume"}
+              subtitle={
+                "View and download my resume to see my professional background and skill sets I've acquired throughout the course of my career."
+              }
+              image={image3}
+            />
+          </Grid>
         </Link>
 
+        <Link target="_blank" href="mailto:israel.varela78@yahoo.com">
+          <Grid item xs={6} sm={3}>
+            <CustomCard
+              classes={styles}
+              title={"Feel Free To Reach Out!"}
+              subtitle={"p: 214-677-7668"}
+              subtitle2={"e: israel.varela78@yahoo.com"}
+              image={image4}
+            />
+          </Grid>
+        </Link>
       </Grid>
-    </>
+    </div>
   );
 });
 export default ContactMe;
