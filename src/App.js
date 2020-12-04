08b99d20a977be8228e2a127e8f32822f736281e
Izzy V <israelreyvarela@gmail.com>
2020-12-04 00:03:17 -0600

    updated / page

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
       <div className="App">
         <Header />
         <Switch>
+        <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/contact" component={Contact} />
